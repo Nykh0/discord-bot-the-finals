624a018d15eceae2f39d926d4687de10fc4e327b
Nykho <nicostur@gmail.com>
2024-06-18 22:57:43 +0200

    Added world tour to the bot

--- commands/utility/rank-world-tour.js
+++ commands/utility/rank-world-tour.js
@@ -2,41 +2,29 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('rank')
-		.setDescription('Check THE FINALS player rank in season 3')
+		.setName('rankwt')
+		.setDescription('Check THE FINALS player rank in World Tour')
 		.addStringOption(option =>
 			option.setName("embark-id")
 				.setDescription('Embark ID of the player (Format : Username#9999)').setRequired(true)),
 	async execute(interaction) {
 		await interaction.deferReply();
-		fetch('https://id.embark.games/_next/data/B-lkaPSkbNrJQwY3fq9CX/en/leaderboards/ranked.json?slug=ranked')
+		fetch('https://id.embark.games/_next/data/B-lkaPSkbNrJQwY3fq9CX/en/leaderboards/world-tour.json?slug=world-tour')
         .then(response => response.json())
         .then(fullJson => {
 			const data = fullJson.pageProps.entries;
             let player = data.find((elmt) => elmt.name.toLowerCase() === interaction.options.getString('embark-id').toLowerCase());
             if (player){
-				const diff = player.r - player.or;
-                if (diff < 0){
-                    player.classDiff = "positive";
-                    player.diff = diff.toString().replace("-","+");
-                } else if (diff > 0) {
-                    player.classDiff = "negative";
-                    player.diff = "-" + diff;
-                } else {
-                    player.classDiff = "zero";
-                    player.diff = "0";
-                }
-				player.nameRank = fullJson.pageProps.metadata.rankToLeagueMap[player.ri].label;
-				player.imgRankName = fullJson.pageProps.metadata.rankToLeagueMap[player.ri].imagePath;
-
+				const formatter = new Intl.NumberFormat('en-US', {
+					style: 'currency',
+					currency: 'USD',
+				});
 				const fields = [{ name: 'Rank', value: player.r.toString(), inline: true },
-				{ name: 'League', value: player.nameRank, inline: true },
-				{ name: '24h', value: player.diff, inline: true }];
+				{ name: 'Cash', value: formatter.format(player.p), inline: true }];
 
 				const embed = new EmbedBuilder()
 				.setColor(0xd31f3c)
 				.setTitle(getName(player))
-				.setThumbnail("https://id.embark.games" + player.imgRankName)
 				.addFields(fields);
 
 				return interaction.editReply({ embeds: [embed] });
